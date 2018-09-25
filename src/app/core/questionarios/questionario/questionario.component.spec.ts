4e88ac7f8775a4dcbb6f6b6a22a5a8ef71d9eba6
Daniel Leonardo de Souza Teixeira <daniel.fnz@hotmail.com>
2018-09-25 15:28:41 -0300

    Update questionario.component.spec.ts

--- src/app/core/questionarios/questionario/questionario.component.spec.ts
+++ src/app/core/questionarios/questionario/questionario.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuestionarioComponent } from './questionario.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '../../../shared/shared.module';
+import { QuestionarioRespondidoComponent } from '../questionario-respondido/questionario-respondido.component';
 
 describe('QuestionarioComponent', () => {
   let component: QuestionarioComponent;
@@ -9,8 +11,8 @@ describe('QuestionarioComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [QuestionarioComponent],
-      imports: [RouterTestingModule],
+      declarations: [QuestionarioComponent,QuestionarioRespondidoComponent],
+      imports: [RouterTestingModule, SharedModule],
     })
     .compileComponents();
   }));
